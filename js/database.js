75ef884ff9c4f3e70fc0299432e4929b21c11427
Romerison M. C. Junior <romerisonjunior@gmail.com>
2023-01-30 15:47:05 -0300

    Add DataBase em JS

---
+++ js/database.js
@@ -0,0 +1,162 @@
+const data =[
+    {
+        id: 0,
+        tag: 'Dados',
+        image: 'dados_1.jpeg',
+        title: 'Dados Metálicos',
+        desc: 'Dados feitos de cobre polido e pindado de roxo',
+        price: 123.99
+    },
+    {
+        id: 1,
+        tag: 'Dados',
+        image: 'dados_2.jpeg',
+        title: 'Dados Sangrentos',
+        desc: 'Dados de metal estilo dark com design de pirata. Para aventuras sombrias e em alto mar.',
+        price: 100.00
+    },
+    {
+        id: 2,
+        tag: 'Dados',
+        image: 'dados_3.jpeg',
+        title: 'Dados Gárgulas',
+        desc: 'Dados estilo pele de gárgula para os apreciadores de aventuras Old School Terror',
+        price: 98.99
+    },
+    {
+        id: 3,
+        tag: 'Dados',
+        image: 'dados_4.jpeg',
+        title: 'Dados Rúinicos',
+        desc: 'Dados com runas entalhadas, para aventuras que presam de destaque nas rolagem.',
+        price: 99.99
+    },
+    {
+        id: 4,
+        tag: 'Dados',
+        image: 'dados_5.jpeg',
+        title: 'Dados Comuns',
+        desc: 'Dados clássicos para PRG de mesa, feitos de plástico ',
+        price: 30.00
+    },    
+    {
+        id: 5,
+        tag: 'Miniaturas',
+        image: 'miniatura_1.jpeg',
+        title: 'Guerreira Tiefling',
+        desc: 'Miniatura de uma guerreira tiefling de 10cm feita de plástico',
+        price: 13.00
+    },
+    {
+        id: 6,
+        tag: 'Miniaturas',
+        image: 'miniatura_2.jpeg',
+        title: 'Principe Inferior',
+        desc: 'Um dos princípes demoniacos de Forgotten Realms com 19cm feito em impressora 3d',
+        price: 25.99
+    },
+    {
+        id: 7,
+        tag: 'Miniaturas',
+        image: 'miniatura_3.jpeg',
+        title: 'Guerreiro Orc',
+        desc: 'Guerreiro Orc de 10cm feito em impressora 3d',
+        price: 18.00
+    },
+    {
+        id: 8,
+        tag: 'Miniaturas',
+        image: 'miniatura_4.jpeg',
+        title: 'Esfinge',
+        desc: 'Monstro Esfinge para aventuras de RPG feito em impressora 3d, possui 20cm',
+        price: 30.00
+    },
+    {
+        id: 9,
+        tag: 'Miniaturas',
+        image: 'miniatura_5.jpeg',
+        title: 'Conjunto Épico',
+        desc: 'Conjunto de miniaturas de monstros para aventuras épicas de RPG de mesa.',
+        price: 130.00
+    },    
+    {
+        id: 10,
+        tag: 'Escudos',
+        image: 'escudo_1.jpeg',
+        title: 'Escudo de Cthulhu',
+        desc: 'Escudo do mestre feito em madeira, com entalhe de Cthulhu',
+        price: 150.00
+    },
+    {
+        id: 11,
+        tag: 'Escudos',
+        image: 'escudo_2.jpeg',
+        title: 'Escudo D&D',
+        desc: 'Escudo do meste feito em papel cartão com decoração de princiapal cenário de D&D',
+        price: 50.00
+    },
+    {
+        id: 12,
+        tag: 'Escudos',
+        image: 'escudo_3.jpeg',
+        title: 'Escudo de Chamas',
+        desc: 'Escudo do mestre feito de aço e com design de câmeras',
+        price: 80.00
+    },
+    {
+        id: 13,
+        tag: 'Escudos',
+        image: 'escudo_4.jpeg',
+        title: 'Escudo com Torres',
+        desc: 'Escudo do mestre feito de MDF com duas torres para rolagem de dados.',
+        price: 120.00
+    },
+    {
+        id: 14,
+        tag: 'Escudos',
+        image: 'escudo_5.jpeg',
+        title: 'Escudo de D&D',
+        desc: 'Escudo do Mestre feito de madeira e entalhado com a logo de Dungeons & Dragons',
+        price: 130.00
+    },    
+    {
+        id: 15,
+        tag: 'Mapas',
+        image: 'mapa_1.jpg',
+        title: 'Mapa de Faerûn',
+        desc: 'Mapa original de Faerûn do cenário de Forgotten Realms 39x50cm',
+        price: 189.99
+    },
+    {
+        id: 16,
+        tag: 'Mapas',
+        image: 'mapa_2.jpg',
+        title: 'Mapa de Glockth',
+        desc: 'Mapa de Glockth mundo de fantasia medieval 45x60cm',
+        price: 90.00
+    },
+    {
+        id: 17,
+        tag: 'Mapas',
+        image: 'mapa_3.jpg',
+        title: 'Mapa do Mar de Espadas',
+        desc: 'Mapa da Costa das Espadas, oeste de Farûn 60x73',
+        price: 179.59
+    },
+    {
+        id: 18,
+        tag: 'Mapas',
+        image: 'mapa_4.jpg',
+        title: 'Mapa de The Witcher',
+        desc: 'Mapa decorativo do mundo de The Witcher 30x45',
+        price: 59.90
+    },
+    {
+        id: 19,
+        tag: 'Mapas',
+        image: 'mapa_5.jpeg',
+        title: 'Batter Board',
+        desc: 'Conjunto de mapas de batalhas para personalizar 10 folhas 30x30 pinceis e cards personalizados',
+        price: 100.00
+    }
+    ]
